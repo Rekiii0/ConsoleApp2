10f95f76778b5d0a10132a552abbef854c5a8d31
user <user@DESKTOP-K9TBN0B>
2024-10-26 21:04:23 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class vehicle
+    {
+        public string brand;
+    }
+        class MotorVehicle : vehicle
+        {
+            public string modelName;
+        }
+        class Car : MotorVehicle
+        {
+
+        }
+        class truck : MotorVehicle
+        {
+
+        }
+    
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Car MyCar = new Car();
+            MyCar.brand = "kai";
+            MyCar.modelName = "sportage";
+            truck MyTruck = new truck();
+            MyTruck.brand = "volvo";
+            MyTruck.modelName = "FH";
+            Console.WriteLine("my car brand and modelm is" + MyCar.brand + "and" + MyCar.modelName);
+            Console.WriteLine("my truck brand and modelm is" + MyTruck.brand + "and" + MyTruck.modelName);
+            Console.ReadLine();
+        }
+    }
+}
